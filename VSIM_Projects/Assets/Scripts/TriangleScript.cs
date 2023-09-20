e873f832b25e5c10b50d7036b2d7552b80af6836
Bjørn Joachim Olsen <jkingolsen@gmail.com>
2023-09-20 14:37:29 +0100

    Vertices being read

--- VSIM_Projects/Assets/Scripts/TriangleScript.cs
+++ VSIM_Projects/Assets/Scripts/TriangleScript.cs
@@ -1,16 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class TriangleScript : MonoBehaviour
 {
 
-    [SerializeField] TextAsset TriangleInfo;
+    [SerializeField] TextAsset TriangleVertices;
+    [SerializeField] TextAsset TriangleIndices;
 
     Vector3 NormalVector = new Vector3(0.0f, 0.0f, 0.0f);
-    Vector3[] Vertices;
+    List<Vector3> Vertices = new List<Vector3>();
     int[] Indices;
     // Start is called before the first frame update
     void Awake()
@@ -26,22 +31,14 @@ public class TriangleScript : MonoBehaviour
         // };
 
         // filter.sharedMesh = mesh;
-        Debug.Log(TriangleInfo.text);
-        Debug.Log(TriangleInfo.text.ToString());
-        Debug.Log(TriangleInfo.dataSize);
+       //Debug.Log(TriangleInfo.text);
+       //Debug.Log(TriangleInfo.text.ToString());
+       // Debug.Log(TriangleInfo.dataSize);
 
-        for (int i = 0; i < TriangleInfo.dataSize; i++)
+        FetchVertices();
+        for (int i = 0; i < Vertices.Count; i++)
         {
-            string lineCheck = TriangleInfo.ToString()[i].ToString();
-            if (i + 1 < TriangleInfo.dataSize)
-            {
-                lineCheck.Concat(TriangleInfo.ToString()[i + 1].ToString());
-            }
-
-            if (lineCheck == "\n")
-            {
-                Debug.Log("testing");
-            }
+            Debug.Log("(" + Vertices[i][0] + ", " + Vertices[i][1] + ", " + Vertices[i][2] + ")");
         }
     }
 
@@ -59,17 +56,27 @@ public class TriangleScript : MonoBehaviour
     
     void FetchVertices()
     {
-        for(int i = 0; i < 5; i++)
+        string text = TriangleVertices.text;
+
+        Vector3 Vertex;
+        List<string> lines = text.Split(new string[] {"\r\n","\r", "\n"}, StringSplitOptions.RemoveEmptyEntries).ToList<string>();
+        List<string> stringVertex = new List<string>();
+
+        for (int i = 0; i < lines.Count; i++)
         {
-            
+            stringVertex = lines[i].Split(new char[] {'(', ',', ')'}, StringSplitOptions.RemoveEmptyEntries).ToList<string>();
+
+            float x = float.Parse(stringVertex[0], CultureInfo.InvariantCulture);
+            float y = float.Parse(stringVertex[1], CultureInfo.InvariantCulture);
+            float z = float.Parse(stringVertex[2], CultureInfo.InvariantCulture);
+            Vertex = new Vector3(x, y, z);
+            Vertices.Add(Vertex);
         }
+
     }
 
     void FetchIndices()
     {
-        for(int i = 0; i < 5; i++)
-        {
-            
-        }
+        
     }
 }
